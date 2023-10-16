4d983c28a00e5aa98bf924a92e71205718bf884b
Iban Lopez <lopez.iban@gmail.com>
2023-10-16 09:09:17 +0000

    fix: undersocred link for accessibility

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer className="text-neutral-600 pt-10 pb-4 px-4">
-            <p className="text-center text-xs font-semibold">&copy; 2023 Todos los derechos reservados. <Link href="/" className="text-indigo-600">Ibán López</Link></p>
+            <p className="text-center text-xs font-semibold">&copy; 2023 Todos los derechos reservados. <Link href="/" className="text-indigo-600 hover:underline">Ibán López</Link></p>
         </footer>
     )
 }
