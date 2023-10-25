abdaff4cad1f1050d31a7231161dcd57478d1d51
ibanlopez <lopez.iban@gmail.com>
2023-10-25 09:23:14 +0200

    feat: added analytics component to rootLayout to actually meassure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import Header from '@/components/header'
 import './globals.css'
 import type { Metadata } from 'next'
@@ -7,38 +8,39 @@ import Footer from '@/components/footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  metadataBase: new URL(`${process.env.NEXT_PUBLIC_HOSTNAME}`),
-  referrer: 'origin-when-cross-origin',
-  creator: 'Ibán López',
-  publisher: 'Ibán López',
-  authors: [{name: 'Ibán López'}],
-  themeColor: '#4f46e5',
-  robots: {
-      index: false,
-      follow: false,
-      noarchive: true,
-      nocache: true,
-      noimageindex: true,
-      nosnippet: true,
-      nositelinkssearchbox: true,
-  }
+	metadataBase: new URL(`${process.env.NEXT_PUBLIC_HOSTNAME}`),
+	referrer: 'origin-when-cross-origin',
+	creator: 'Ibán López',
+	publisher: 'Ibán López',
+	authors: [{ name: 'Ibán López' }],
+	themeColor: '#4f46e5',
+	robots: {
+		index: false,
+		follow: false,
+		noarchive: true,
+		nocache: true,
+		noimageindex: true,
+		nosnippet: true,
+		nositelinkssearchbox: true,
+	},
 }
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-
-  return (
-    <html lang="es" className="overflow-x-hidden">
-      <body className={`${inter.className} relative overflow-x-hidden min-h-screen flex flex-col flex-wrap max-w-screen bg-slate-100`}>
-        <Header />
-        <main className="grow pt-10 px-4 break-words hyphens-auto">
-          {children}
-        </main>
-        <Footer />
-      </body>
-    </html>
-  )
+	return (
+		<html lang="es" className="overflow-x-hidden">
+			<body
+				className={`${inter.className} relative overflow-x-hidden min-h-screen flex flex-col flex-wrap max-w-screen bg-slate-100`}>
+				<Header />
+				<main className="grow pt-10 px-4 break-words hyphens-auto">
+					{children}
+				</main>
+				<Footer />
+				<Analytics />
+			</body>
+		</html>
+	)
 }
